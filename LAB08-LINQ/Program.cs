fdde659afa87d2a90729aed85a92d2a9374b2e99
Chris <chrismorton.cm@gmail.com>
2019-03-31 19:01:20 -0700

    Add project files.

---
+++ LAB08-LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAB08_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
