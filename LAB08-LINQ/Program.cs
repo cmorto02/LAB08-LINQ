942d831a38d582a9f19184d7ea70c3e376cb5183
Chris <chrismorton.cm@gmail.com>
2019-03-31 21:04:29 -0700

    added photos and summary comments

--- LAB08-LINQ/Program.cs
+++ LAB08-LINQ/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace LAB08_LINQ
 {
@@ -6,7 +9,116 @@ namespace LAB08_LINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("No dupes or blanks---------------------");
+            AllThree();
+            Console.WriteLine("No dupes---------------------");
+            RemoveDuplicates();
+            Console.WriteLine("No blanks---------------------");
+            FilterNeighborhoods();
+            Console.WriteLine("Raw---------------------");
+            GetAllNeighborhoods();
+
+
+        }
+        /// <summary>
+        /// This method deserializes json data and then applies a LINQ query to get the neighborhood info
+        /// </summary>
+        static void GetAllNeighborhoods()
+        {
+            string path = "../../../data.json";
+            var JSONData = "";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                JSONData = sr.ReadToEnd();
+            }
+            Rootobject root = JsonConvert.DeserializeObject<Rootobject>(JSONData);
+            var query = from neighborhoods in root.features
+                        select neighborhoods.properties.neighborhood;
+            foreach(string neighborhood in query)
+            {
+                Console.WriteLine(neighborhood);
+            }
+
+        }
+        /// <summary>
+        /// This method deserializes json data and then applies a LINQ query to get the neighborhood info without places that dont have a name
+        /// </summary>
+        static void FilterNeighborhoods()
+        {
+            string path = "../../../data.json";
+            var JSONData = "";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                JSONData = sr.ReadToEnd();
+            }
+            Rootobject root = JsonConvert.DeserializeObject<Rootobject>(JSONData);
+            var query = from neighborhoods in root.features
+                        where (neighborhoods.properties.neighborhood != "")
+                        select neighborhoods.properties.neighborhood;
+            foreach(string neighborhood in query)
+            {
+                Console.WriteLine(neighborhood);
+            }
+        }
+        /// <summary>
+        /// This method deserializes json data and then applies a LINQ query to get the neighborhood info without duplicates
+        /// </summary>
+        static void RemoveDuplicates()
+        {
+            string path = "../../../data.json";
+            var JSONData = "";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                JSONData = sr.ReadToEnd();
+            }
+            Rootobject root = JsonConvert.DeserializeObject<Rootobject>(JSONData);
+            var query = from neighborhoods in root.features
+                        select neighborhoods.properties.neighborhood;
+            foreach (string neighborhood in query.Distinct())
+            {
+                Console.WriteLine(neighborhood);
+            }
+        }
+        /// <summary>
+        /// This method deserializes json data and then applies a LINQ query to get the neighborhood info without duplicates and without places with no name
+        /// </summary>
+        static void AllThree()
+        {
+            string path = "../../../data.json";
+            var JSONData = "";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                JSONData = sr.ReadToEnd();
+            }
+            Rootobject root = JsonConvert.DeserializeObject<Rootobject>(JSONData);
+            var query = from neighborhoods in root.features
+                        where (neighborhoods.properties.neighborhood != "")
+                        select neighborhoods.properties.neighborhood;
+            foreach (string neighborhood in query.Distinct())
+            {
+                Console.WriteLine(neighborhood);
+            }
+        }
+
+        /// <summary>
+        /// This method deserializes json data and then applies a LINQ query to get the neighborhood info without duplicates
+        /// </summary>
+        static void RemoveDupesOtherWay()
+        {
+            string path = "../../../data.json";
+            var JSONData = "";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                JSONData = sr.ReadToEnd();
+            }
+            Rootobject root = JsonConvert.DeserializeObject<Rootobject>(JSONData);
+
+            var query = root.features.Select(n => n.properties.neighborhood).Distinct();
+
+            foreach (var n in query)
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
