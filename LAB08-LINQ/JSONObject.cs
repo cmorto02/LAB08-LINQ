d38dabb87122dde577c90e953283a121d879f57f
Chris <chrismorton.cm@gmail.com>
2019-03-31 19:23:17 -0700

    slasses created

---
+++ LAB08-LINQ/JSONObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB08_LINQ
+{
+
+        public class Rootobject
+        {
+            public string type { get; set; }
+            public Feature[] features { get; set; }
+        }
+
+}
